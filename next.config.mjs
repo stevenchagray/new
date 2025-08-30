c55e70bbec66860636ed5878ad7330329e4bb938
StevenMSI <geminis_scott@hotmail.com>
2025-08-30 14:11:58 -0500

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  assetPrefix: "/new",
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-  output: "export",
 };
 
 export default nextConfig;
